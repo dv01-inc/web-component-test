104789ec76cc70c5a6719084b71a5e671837bfa0
Denis Molloy <denis.molloy@gmail.com>
2025-01-08 18:16:49 -0600

    Adding typescript support for wc.  Both js and type need to be imported

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import dts from 'vite-plugin-dts'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'src/web-component-types.d.ts', dest: '' },
+      ],
+    }),
+    // TODO: better to use DTS files for typescript types?  this currently outputs an empty file
+    // dts({
+    //   outDir: 'dist/types',
+    //   insertTypesEntry: true, // Automatically adds a "types" entry in package.json
+    //   tsconfigPath: './tsconfig.json'
+    // }),
+  ],
   build: {
     lib: {
       entry: 'src/index.ts',
