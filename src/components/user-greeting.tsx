f6d2976397a361bc12105282ba2880ce39b19d9c
Denis Molloy <denis.molloy@gmail.com>
2025-01-10 14:47:54 -0600

    waterfall styles getting picked up by wc's

--- src/components/user-greeting.tsx
+++ src/components/user-greeting.tsx
@@ -1,5 +1,5 @@
 const UserGreeting = ({ name }: {name:string}) => {
-  return <div>Hello, {name}!</div>;
+  return <div className='my-4'>Hello, {name ? name : "Nameless One 🫣"}!</div>;
 };
 
 export default UserGreeting;
--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ import { viteStaticCopy } from 'vite-plugin-static-copy'
 export default defineConfig({
   plugins: [
     react(),
-    // copy the custom-element-types.d.ts file to the dist folder
+    // copy the dv01-web-components.d.ts file to the dist folder
     viteStaticCopy({
       targets: [
-        { src: 'src/custom-element-types.d.ts', dest: '' },
+        { src: 'src/dv01-web-components.d.ts', dest: '' },
       ],
     }),
     // FIXME: better to use DTS files for typescript types?  this currently outputs an empty file
@@ -21,26 +21,27 @@ export default defineConfig({
   ],
   build: {
     lib: {
-      //all custom elements defined in this file will be bundled and defined into custom-elements.[es|umd].js
+      //all custom elements defined in this file will be bundled and defined into dv01-web-components.[es|umd].js
       entry: 'src/defineCustomElements.ts',
       name: 'CustomElements',
-      fileName: (format) => `custom-elements.${format}.js`,
+      fileName: (format) => `dv01-web-components.${format}.js`,
       formats: ['es', 'umd'],
     },
-    rollupOptions: {
-      // FIXME: external option - rely on consumer to have react and react-dom installed - but this results in error when trying to use the umd file
-      // UMD error: Cannot read properties of undefined (reading '__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED') <- react error
-      // size with the external property:
-      // included: es - 3.3mb, umd - 3.1mb
-      // excluded: es - 4mb, umd - 3.6mb
-      // external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
+    // not externalizing to encapsulate packages needed for the web components
+    // rollupOptions: {
+    //   // external option - rely on consumer to have react and react-dom installed - but this results in error when trying to use the umd file
+    //   // UMD error: Cannot read properties of undefined (reading '__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED') <- react error
+    //   // size with the external property:
+    //   // included: es - 3.3mb, umd - 3.1mb
+    //   // excluded: es - 4mb, umd - 3.6mb
+    //   external: ['react', 'react-dom'],
+    //   output: {
+    //     globals: {
+    //       react: 'React',
+    //       'react-dom': 'ReactDOM',
+    //     },
+    //   },
+    // },
   },
   resolve: {
     alias: {
